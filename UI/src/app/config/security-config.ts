0c87911b09720004464567d63431ea62a5e45bc6
Surendiran S <surenyas1902@gmail.com>
2021-12-13 20:19:14 +0530

    Security Package added

---
+++ UI/src/app/config/security-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa37yok3ph2GroGP5d7',
+        issuer: 'https://dev-74426722.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
